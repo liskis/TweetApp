aabcd0f7b6fa0bdc32b334f67f834061b2db3663
liskis <watanabe@liskis.org>
2023-12-07 11:08:08 +0900

    xibでtableCellを作る

---
+++ TweetApp/TweetApp/HomeViewController.swift
@@ -0,0 +1,53 @@
+//
+//  HomeViewController.swift
+//  TweetApp
+//
+//  Created by 渡辺健輔 on 2023/12/06.
+//
+
+
+import Foundation
+import UIKit
+
+class HomeViewController: UIViewController, UITableViewDelegate {
+    @IBOutlet weak var tweetView: UITableView!
+    var tweetDataList: [TweetDataModel] = []
+    
+    override func viewDidLoad() {
+        tweetView.dataSource = self
+        tweetView.delegate = self
+        tweetView.rowHeight = UITableView.automaticDimension
+        tweetView.tableFooterView = UIView()
+        tweetView.register(UINib(nibName: "HomeTableViewCell", bundle: nil), forCellReuseIdentifier: "HomeTableViewCell")
+        setTweetData()
+    }
+    
+    func setTweetData(){
+        for i in 1...5 {
+            let tweetData = TweetDataModel(
+                userName: "testUser",
+                tweet: "このツイートは\(i)番目のツイートです。このツイートは\(i)番目のツイートです。このツイートは\(i)番目のツイートです。",
+                created: Date()
+            )
+            tweetDataList.append(tweetData)
+        }
+    }
+}
+
+extension HomeViewController: UITableViewDataSource{
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+       return 1
+     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tweetDataList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "HomeTableViewCell", for: indexPath ) as! HomeTableViewCell
+        cell.setCell(tweetData: tweetDataList[indexPath.row])
+        return cell
+    }
+}
+
